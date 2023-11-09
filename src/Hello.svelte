5f4ad67dafad5e0f0e808644268adabc36a2048f
ChisomUma <chisomuma476@gmail.com>
2023-11-09 19:32:35 +0100

    sveltereactapp

---
+++ src/Hello.svelte
@@ -0,0 +1,8 @@
+<script>
+    export let extraText = "Default text";
+    export let onClick = () => {};
+</script>
+
+<div class="text-3xl font-bold" on:click={onClick}>
+    Hello from Svelte! {extraText}
+</div>
